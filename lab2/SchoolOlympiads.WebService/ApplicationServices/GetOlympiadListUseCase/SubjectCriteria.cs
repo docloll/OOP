f125ea9bdd1b571c6c6dfb756a3633ac36a5e809
docloll <50889392+docloll@users.noreply.github.com>
2020-05-19 02:32:40 +0300

    lab2

---
+++ lab2/SchoolOlympiads.WebService/ApplicationServices/GetOlympiadListUseCase/SubjectCriteria.cs
@@ -0,0 +1,21 @@
+﻿using SchoolOlympiads.DomainObjects;
+using SchoolOlympiads.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SchoolOlympiads.ApplicationServices.GetOlympiadListUseCase
+{
+    public class SubjectCriteria : ICriteria<Olympiad>
+    {
+        public string Subject { get; }
+
+        public SubjectCriteria(string subject)
+            => Subject = subject;
+
+        public Expression<Func<Olympiad, bool>> Filter
+            => (s => s.Subject == Subject);
+    }
+}
---
+++ lab2/SchoolOlympiads.WebService/DomainObjects/Olympiad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolOlympiads.DomainObjects
+{
+    public class Olympiad : DomainObject
+    {
+        public string Name { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Type { get; set; }
+
+        public string Class { get; set; }
+
+        public int Year { get; set; } 
+
+    }
+}
