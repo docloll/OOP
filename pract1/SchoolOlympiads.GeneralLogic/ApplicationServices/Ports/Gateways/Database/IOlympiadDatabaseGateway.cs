fab9f874a3642e7bb1fa08f98ddae65a11e9f97a
docloll <50889392+docloll@users.noreply.github.com>
2020-06-01 20:53:39 +0300

    Add files via upload

---
+++ pract1/SchoolOlympiads.GeneralLogic/ApplicationServices/Ports/Gateways/Database/IOlympiadDatabaseGateway.cs
@@ -0,0 +1,24 @@
+﻿using SchoolOlympiads.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SchoolOlympiads.ApplicationServices.Ports.Gateways.Database
+{
+    public interface IOlympiadDatabaseGateway
+    {
+        Task AddOlympiad(Olympiad olympiad);
+
+        Task RemoveOlympiad(Olympiad olympiad);
+
+        Task UpdateOlympiad(Olympiad olympiad);
+
+        Task<Olympiad> GetOlympiad(long id);
+
+        Task<IEnumerable<Olympiad>> GetAllOlympiads();
+
+        Task<IEnumerable<Olympiad>> QueryOlympiads(Expression<Func<Olympiad, bool>> filter);
+
+    }
+}
---
+++ pract1/SchoolOlympiads.WebService/InfrastructureServices/Presenters/OlympiadListPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using SchoolOlympiads.ApplicationServices.Ports;
+using SchoolOlympiads.ApplicationServices.GetOlympiadListUseCase;
+
+namespace SchoolOlympiads.InfrastructureServices.Presenters
+{
+    public class OlympiadListPresenter : IOutputPort<GetOlympiadListUseCaseResponse>
+    {
+        public JsonContentResult ContentResult { get; }
+
+        public OlympiadListPresenter()
+        {
+            ContentResult = new JsonContentResult();
+        }
+
+        public void Handle(GetOlympiadListUseCaseResponse response)
+        {
+            ContentResult.StatusCode = (int)(response.Success ? HttpStatusCode.OK : HttpStatusCode.NotFound);
+            ContentResult.Content = response.Success ? JsonConvert.SerializeObject(response.Olympiads) : JsonConvert.SerializeObject(response.Message);
+        }
+    }
+}
